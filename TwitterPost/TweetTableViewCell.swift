8b6ec7e1a2df3aa1ce6d9f3083afa360aa143de9
Omar Faruqe <faruqe@Omars-MacBook-Pro.local>
2015-06-07 19:01:28 -0400

    first commit

---
+++ TwitterPost/TweetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetTableViewCell.swift
+//  TwitterPost
+//
+//  Created by Omar Faruqe on 2015-06-07.
+//  Copyright (c) 2015 Omar Faruqe. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
